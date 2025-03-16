bb82caa2c697262c77cc72646ffe6d70d849d194
Aashutosh Pudasaini <nepalidude3@gmail.com>
2025-03-16 18:12:57 +0545

    add: test for create note

--- apps/api/src/modules/note/router.ts
+++ apps/api/src/modules/note/router.ts
@@ -2,11 +2,12 @@ import { Hono } from "hono";
 import NoteController from "./controller";
 import NoteService from "./service";
 import { getCurrentUser } from "../auth/middlewares/getCurrentUser";
+import noteCreationValidator from "./entities/requests/note";
 
 const noteRouter = new Hono();
 
 const noteController = new NoteController(await NoteService.NewNoteService());
 
-noteRouter.post("/", getCurrentUser, noteController.createNote);
+noteRouter.post("/", getCurrentUser,noteCreationValidator, noteController.createNote);
 
 export default noteRouter;
